41b9356a8974f917cb5405dec623aab5c751581b
santanu1931 <santanu1931@users.noreply.github.com>
2024-03-06 20:10:51 +0530

    first commit

---
+++ src/main/java/com/codingtips/SwatantraHospitalsApplication.java
@@ -0,0 +1,15 @@
+package com.codingtips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.w3c.dom.Document;
+
+@SpringBootApplication
+public class SwatantraHospitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwatantraHospitalsApplication.class, args);
+		System.out.print("welcome");
+	}
+
+}
